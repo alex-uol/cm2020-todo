c48367c6392f23ac46812ef25a03bfdb2857b70d
Oleksandr Allakhverdiyev <135544061+alex-uol@users.noreply.github.com>
2024-07-15 01:49:29 -0700

    feat: Initial app code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# cm2020-todo
+# Local installation
+
+Make sure you have the latest Node and NPM.
+
+```bash
+# Clone the code
+git clone https://github.com/alex-uol/todo-app
+cd todo-app
+# Install node packages
+npm install
+# Run local server
+npm run server
+```
+
+The application should be accessible on http://localhost:9000/
+
+# File structure
+
+`public/` - static files such as HTML and CSS.
+`src/` - JavaScript source code for the application.
+`webpack.config.js` - configuration for the Webpack code bundler and dev web server.
+`babel.config.json` - Configuration for the Babel.js compiler.
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", // Entry point for your application
+  output: {
+    filename: "app.js", // Name of the output bundle
+    path: path.resolve(__dirname, "public/dist"), // Output directory
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/, // Apply Babel transpilation to JavaScript files
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+      // Add other rules here (e.g., for CSS or images)
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"), // Serve files from the dist directory
+      serveIndex: false,
+    },
+    port: 9000,
+  },
+};
