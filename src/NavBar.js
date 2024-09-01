e466328ec054e1aa0059df326657a36a7665d10d
ping7227 <110403729+ping7227@users.noreply.github.com>
2024-09-02 00:11:13 +0800

    Feature/enhanced sorting (#40)
    * added simple category, tag and search features
    
    * Refactor with more components, add sorting functions
    
    ---------
    
    Co-authored-by: Ming1932 <suikyouyukari@gmail.com>

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,10 +6,12 @@ function NavBar({ setContext }) {
       <ul>
         <li onClick={() => setContext('list')}>All Tasks</li>
         <li onClick={() => setContext('create')}>Create Task</li>
-        <li onClick={() => setContext('categorieManager')}>Categorie Manager</li>
+        <li onClick={() => setContext('categorieManager')}>Category Manager</li>
         <li onClick={() => setContext('tagManager')}>Tag Manager</li>
         <li className="divider"></li>
         <li onClick={() => setContext("dataManagement")}>Export / Import</li>
+        <li className="divider"></li>
+        <li onClick={() => setContext("resetApp")}>Reset Application</li>
       </ul>
     </nav>
   );
---
+++ src/SubTaskList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * SubTaskList Component
+ * Renders a list of sub-tasks.
+ * 
+ * @param {Object} props
+ * @param {Object[]} props.subTasks - List of sub-task objects.
+ * @returns {JSX.Element} A rendered sub-task list.
+ */
+
+const SubTaskList = ({ subTasks }) => {
+  return (
+    <ul className="sub-task-list">
+      {subTasks && subTasks.length > 0 && subTasks.map(subTask => (
+        <li key={subTask.id}>{subTask.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default SubTaskList;
